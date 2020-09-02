777383921bafbd88bb5816932a966ef734987592
Tatiana <tatiana.martinez.ironhack@gmail.com>
2020-09-02 09:04:54 +0200

    new version

--- Labs/module_1/Advanced-mySQL/solutions.sql
+++ Labs/module_1/Advanced-mySQL/solutions.sql
@@ -54,7 +54,32 @@ GROUP BY Title_ID, Author_ID;
 CREATE TEMPORARY TABLE publications.step2
 SELECT Title_ID, Author_ID, SUM(Royalty) AS Royalty, Advance
 FROM step1
-GROUP BY Title_ID, Author_ID;
+GROUP BY Title_ID, Author_ID,Advance;
 
+SELECT Author_ID,SUM(Advance), SUM(Royalty)
+FROM step2
+GROUP BY Author_ID;
 
+#challenge3
+CREATE TEMPORARY TABLE publications.step3
+SELECT Author_ID,SUM(Advance) As SAvance, SUM(Royalty) As SRoyalty
+FROM step2
+GROUP BY Author_ID;
+
+SELECT * FROM step3;
+
+SELECT Author_ID,
+(SAvance +SRoyalty) As profits
+FROM step3
+ORDER BY profits DESC
+LIMIT 3;
+
+CREATE TABLE most_profiting_authors
+SELECT Author_ID,
+(SAvance +SRoyalty) As profits
+FROM step3
+ORDER BY profits DESC
+LIMIT 3;
+
+SELECT * FROM most_profiting_authors;
 
